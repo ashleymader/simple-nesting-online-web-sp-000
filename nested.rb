d9ed3edb181aed256cb7a04e91b8fce144562ed7
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 16:15:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -22,6 +22,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	alan_known_for = [:alan_kay][:known_for]
+	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     alan_known_for
 end
 
 def dennis_ritchies_language
