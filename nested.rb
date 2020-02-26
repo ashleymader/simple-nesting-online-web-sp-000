f2e2cc3ddbc0f4d7c8b8c3f6b0d472168e897567
Ashley M Mader <ashley.mader12@gmail.com>
2020-02-26 17:00:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,8 +130,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
-end
-new_language = "Assembly"
 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
+
+end
+
